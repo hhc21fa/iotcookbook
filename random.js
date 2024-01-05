76d1c0cbb091264dfc1c68a7b09228ae0126e984
hhc21fa <hzhwang@gmail.com>
2024-01-05 10:01:52 +0800

    Update random.js

--- random.js
+++ random.js
@@ -1,9 +1,9 @@
 
 function genASCIIString(len) {
-const hex = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let output = '';
-for (let i = 0; i < len; ++i) {
-  output += hex.charAt(Math.floor(Math.random() * hex.length));
-}
-return output;
+  const hex = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let output = '';
+  for (let i = 0; i < len; ++i) {
+    output += hex.charAt(Math.floor(Math.random() * hex.length));
+  }
+  return output;
 }
